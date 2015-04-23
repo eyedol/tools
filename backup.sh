9faa68d939d90eb0ea07f36ca26c764789261b03
Henry Addo <henry@addhen.org>
2015-04-23 09:31:30 +0900

    Update backup to send notification and an updated ignorelist

--- backup.sh
+++ backup.sh
@@ -6,10 +6,19 @@
 # Don't forget to change the destination path to match where you want 
 # to save the backup to.
 
-DESTINATION="/run/media/$USER/ext4-linux/home/"
+# Use the distribution name and version number to create the folder to 
+# save the backup in. Replace all whitespaces with `-`
+DEST_FOLDER_NAME=$(head -n 1 /etc/issue | tr  ' ' '-')
+
+DESTINATION="/run/media/$USER/ext4-linux/$DEST_FOLDER_NAME/"
+
+# Notification message
+NOTIFY_MSG="Backup finished!"
 
 echo "backing up $USER directory..."
-rsync -aP --exclude-from=/tmp/ignorelist /home/$USER/ $DESTINATION
+rsync -aP --exclude-from=/tmp/ignorelist --relative /home/$USER/ $DESTINATION
 
 echo "backing up /opt/ directory..."
-rsync -aP --exclude-from=/tmp/ignorelist /opt/ $DESTINATION
+rsync -aP --exclude-from=/tmp/ignorelist --relative /opt/ $DESTINATION
+
+notify-send $NOTIFY_MSG -t 10000
